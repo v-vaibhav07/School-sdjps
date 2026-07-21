366266365305706e0a211416c91a3f2596007f62
Vaibhav Yadav <vaibhavyadav6533@gmail.com>
2026-07-21 20:36:11 +0530

    Update admin, student and teacher layouts

--- frontend/src/layouts/StudentLayout.jsx
+++ frontend/src/layouts/StudentLayout.jsx
@@ -1,36 +1,3 @@
-// import { useState } from "react"
-// import { Outlet } from "react-router-dom"
-// import StudentSidebar from "../components/StudentSidebar"
-
-// function StudentLayout() {
-
-//   const [open, setOpen] = useState(true)
-
-//   return (
-//     <div className="flex h-screen overflow-hidden">
-
-//       {/* Sidebar */}
-//       <StudentSidebar open={open} setOpen={setOpen} />
-
-//       {/* Right Side */}
-//       <div className="flex flex-col flex-1 overflow-hidden">
-
-//         {/* Content */}
-//         <div className="flex-1 overflow-y-auto bg-gray-100 p-4">
-//           <Outlet />
-//         </div>
-
-//       </div>
-
-//     </div>
-//   )
-// }
-
-// export default StudentLayout
-
-
-
-
 import { useState } from "react"
 import { Outlet } from "react-router-dom"
 import StudentSidebar from "../components/StudentSidebar"
